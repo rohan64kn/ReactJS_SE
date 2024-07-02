a4ab6b74257f02d3e08a2dfa1a74e533e0cbea7c
rohan64kn <rohan64kn@gmail.com>
2024-07-02 15:33:12 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import One from "./Print('HW')";
+import Two from './Counter';
+import Three from "./User_Input";
+import Four from "./Display_List";
+import Five from "./ToggleSwitch";
+import Six from "./Fetch_API_Data";
+import Seven from "./Timer";
+import Eight from "./Colour_change";
+import Nine from "./Routing";
+import Ten from "./Quotes";
+import Eleven from "./Image";
+import Twelve from "./Login";
+export default function App()
+{
+  return(
+  <div>
+  <One/>
+  <Two/>
+  <Three/>
+  <Four/>
+  <Five/>
+  <Six/>
+  <Seven/>
+  <Eight/>
+  <Nine/>
+  <Ten/>
+  <Eleven/>
+  <Twelve/>
+  </div>
+  )
+}
